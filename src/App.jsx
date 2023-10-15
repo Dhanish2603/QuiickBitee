fa661050bdbc3cd8195fabf5a3d1529172290006
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2023-10-15 11:08:03 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import React,  { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+ 
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-const [cartIsShown,setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-const showCartHandler = () =>{
-  setCartIsShown(true);
-};
-
-const hideCartHandler = () =>{
-  setCartIsShown(false);
-}
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart = {showCartHandler} />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </CartProvider>
   );
